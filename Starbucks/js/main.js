e02655459e8b3141c2f8a77759bbf6de783c3cf0
sung34 <imspins1234@gmail.com>
2022-12-20 05:53:45 +0900

    TIL: Practice lecture Starbucks Clone coding ~13

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -14,3 +14,21 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+ 
+  if (window.scrollY > 500){
+    gsap.to(badgeEl, 0.4, {
+      opacity: 0,
+      display: 'none'
+    });
+  }
+  else{
+    gsap.to(badgeEl, 0.4, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
