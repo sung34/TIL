c36ce90dd95347b523b725a7334bc72f023d2097
sung34 <imspins1234@gmail.com>
2022-12-21 19:04:13 +0900

    TIL: Practice lecture ~30 Starbucks Clone coding

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -32,3 +32,83 @@ window.addEventListener('scroll', _.throttle(function () {
     });
   }
 }, 300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1
+  });
+});
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion){
+    promotionEl.classList.add('hide');
+  }
+  else{
+    promotionEl.classList.remove('hide');
+  }
+});
+
+
+function random(min, max){
+  return parseFloat((Math.random() * (min - max) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, delay, {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0,delay)
+  });
+  
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating1', .5, 15);
+floatingObject('.floating1', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: .8
+  })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
